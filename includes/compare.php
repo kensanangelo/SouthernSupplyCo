2674988cfd2603b9856b3e19a82eaf7a6ba1414c
Ken San Angelo <k.sanangelo@knights.ucf.edu>
2014-10-31 20:01:55 -0400

    Added login logic. Still have to do create account and hashing

--- includes/compare.php
+++ includes/compare.php
@@ -1,22 +1,50 @@
 <?php
 
 
-function compareInfo($user, $pass){
-	echo "IT WORKED!>!!>!> :)))))";
+function loginCheck($user, $pass){
+    include 'CRUD.php';
+
+	$DBuserInfo=mysqli_fetch_assoc(readFromDB("users", "*", "username='".$user."'"));
+    $DBname=$DBuserInfo['username'];
+    $DBpass=$DBuserInfo['password'];
+
+    if ($user==$DBname && $pass==$DBpass){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    //something posted
+    include 'connectdb.php';
 
     if (isset($_POST['login'])) {
 
         $user=$_POST['username'];
         $pass=$_POST['password'];
+
+        $user = preg_replace("/[^A-Za-z0-9]/", " ", $user);
+        $user = $connection->real_escape_string($user);
+
+        $pass = preg_replace("/[^A-Za-z0-9]/", " ", $pass);
+        $pass = $connection->real_escape_string($pass);
         
-        if($user!=NULL && $pass != NULL)
-	        compareInfo($user,$pass);
-	    else
-	    	echo "FAILED TO ENTER INFO BRO";
+        if($user!=NULL && $pass != NULL){
+            $loginSuccess=loginCheck($user,$pass);
+            //Returns true if they logged in correctly
+            //Put user login logic here
+            if ($loginSuccess==true) {
+                echo "YOU LOGGED IN CORRECTLY";
+            }else{
+                echo "YOU FAILED TO LOGIN IN BRO";
+            }
+
+        }
+        else{
+            //Put login fail code here
+            echo "FAILED TO ENTER INFO BRO";
+        }
 
     } else if (isset($_POST['signup'])){
         echo "SIGNUP WORKED";
