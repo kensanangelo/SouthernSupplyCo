d7ac7b897dd830d1ea4826eae21698ca2306e6fe
Ken San Angelo <k.sanangelo@knights.ucf.edu>
2014-11-01 00:07:20 -0400

    Finished form validation on both ends.

--- js/login.js
+++ js/login.js
@@ -1,15 +1,57 @@
 $('#loginForm').on('submit', function(e) {
 
-	console.log("Test successful");
+	var loginUser = document.getElementById('loginUser');
+	var loginPass = document.getElementById('loginPass');
 
-	var user = document.getElementById('loginUser');
-	var pass = document.getElementById('loginPass');
-	if (!user && !pass){
+	if (loginUser.value==""){
+		document.getElementById('loginError').innerHTML="Please enter your username to continue."
+
 		e.preventDefault();
 		e.returnValue = false;
-		console.log("Failed Bro");
+	}else if (loginPass.value==""){
+		document.getElementById('loginError').innerHTML="Please enter your password to continue."
+
+		e.preventDefault();
+		e.returnValue = false;
 	}else
-		console.log("SWEET SUCCESS");
+		e.returnValue = true;
 
 });
 
+$('#signupForm').on('submit', function(e) {
+
+	var signupUser = document.getElementById('createUser');
+	var signupEmail = document.getElementById('createEmail');
+	var signupPass = document.getElementById('createPass');
+	var signupConfirm = document.getElementById('createConfirm');
+
+	if (signupUser.value==""){
+		document.getElementById('signupError').innerHTML="Please enter your username to continue."
+
+		e.preventDefault();
+		e.returnValue = false;
+	}else if (signupEmail.value==""){
+		document.getElementById('signupError').innerHTML="Please enter your email to continue."
+
+		e.preventDefault();
+		e.returnValue = false;
+	}else if (signupPass.value==""){
+		document.getElementById('signupError').innerHTML="Please enter your password to continue."
+
+		e.preventDefault();
+		e.returnValue = false;
+	}else if (signupConfirm.value==""){
+		document.getElementById('signupError').innerHTML="Please confirm your password to continue."
+
+		e.preventDefault();
+		e.returnValue = false;
+	}else if (signupPass.value!=signupConfirm.value){
+		document.getElementById('signupError').innerHTML="Your password does not match in both fields."
+
+		e.preventDefault();
+		e.returnValue = false;
+	}else{
+		e.returnValue = true;
+	}
+
+});
--- login.php
+++ login.php
@@ -13,25 +13,26 @@
 			<div class="option">
 				<h3>Login to Your Account:</h3>
 				<form id="loginForm" action="includes/compare.php" method="POST">
-					<fieldset>
-    					<legend>Login</legend>
-    					<ul>
-							<li><label class="labelFix" for="loginUser">Username:</label><input type="text" name="loginUser" id="loginUser" placeholder="Username"/></li>
-							<li><label class="labelFix" for="loginPass">Password:</label><input size="13" type="text" name="loginPass" id="loginPass" placeholder="Password"/></li>
-						</ul>
-						<input type="submit" name="login" value="Login" class="btn btn-default complete"/>
-					</fieldset>
-					<fieldset>
-    					<legend>Sign Up</legend>
-    					<ul>
-							<li><label class="labelFix" for="createUser">Username:</label><input type="text" name="createUser" placeholder="Username"/></li>
-							<li><label class="labelFix" for="createEmail">Email:</label><input type="text" name="createEmail" placeholder="Email"/></li>
-							<li><label class="labelFix" for="createPass">Password:</label><input type="text" name="createPass" placeholder="Password"/></li>
-							<li><label class="labelFix" for="createConfirm">Confirm Password:</label><input type="text" name="createConfirm" placeholder="Password"/></li>
-						</ul>
-						<input type="submit" name="signup" value="Sign Up" class="btn btn-default complete"/>
-					</fieldset>
+					<legend>Login</legend>
+					<p id="loginError"></p>
+					<ul>
+						<li><label class="labelFix" for="loginUser">Username:</label><input type="text" name="loginUser" id="loginUser" placeholder="Username" required/></li>
+						<li><label class="labelFix" for="loginPass">Password:</label><input size="13" type="text" name="loginPass" id="loginPass" placeholder="Password" required/></li>
+					</ul>
+					<input type="submit" name="login" value="Login" class="btn btn-default complete"/>
 				</form>
+				<form id="signupForm" action="includes/compare.php" method="POST">
+					<legend>Sign Up</legend>
+					<p id="signupError"></p>
+					<ul>
+						<li><label class="labelFix" for="createUser">Username:</label><input type="text" id="createUser" name="createUser" placeholder="Username" required/></li>
+						<li><label class="labelFix" for="createEmail">Email:</label><input type="text" id="createEmail" name="createEmail" placeholder="Email" required/></li>
+						<li><label class="labelFix" for="createPass">Password:</label><input type="text" id="createPass" name="createPass" placeholder="Password" required/></li>
+						<li><label class="labelFix" for="createConfirm">Confirm Password:</label><input type="text" id="createConfirm" name="createConfirm" placeholder="Password" required/></li>
+					</ul>
+					<input type="submit" name="signup" value="Sign Up" class="btn btn-default complete"/>
+				</form>
+			
 			</div>
 		</div>		
 
