284f24b59ba0e375e95c816c962087c8d1edbbc4
kristenbasco <kristenbasco@gmail.com>
2014-11-03 16:00:37 -0500

    Changed sizes of h3 in the catalog so it doesn't overlap. Added white logo. Color scheme might be temp... will keep changing.

--- header.php
+++ header.php
@@ -16,7 +16,7 @@ $user_access="3"; ?>
 	<div id="header">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-2 brand"><a href="home.php"><img class="logo" src="img/logo.png" alt="Southern Supply Co. Logo"></a>
+			<div class="col-md-2 brand"><a href="home.php"><img class="logo" src="img/logo-white.png" alt="Southern Supply Co. Logo"></a>
 			</div>
 			<div class=" login col-md-2 col-md-offset-8">
 				<?php
